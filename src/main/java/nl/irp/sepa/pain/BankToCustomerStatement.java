df03e0290bfe8d664803c7f8a323e08751e91c40
Jasper Krijgsman <jd.krijgsman@gmail.com>
2012-05-16 13:22:22 +0200

    Start with  Bank to Customer Statement

---
+++ src/main/java/nl/irp/sepa/pain/BankToCustomerStatement.java
@@ -0,0 +1,10 @@
+package nl.irp.sepa.pain;
+
+/**
+ * ISO20022 - "camt.053.001.02" in the Netherlands.
+ * @author "Jasper Krijgsman <jasper@irp.nl>"
+ *
+ */
+public class BankToCustomerStatement {
+
+}
